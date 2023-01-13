d4aedd46fa4d50fe4903f9d68557844e1526eea4
Ali Alwashali <a.alwashli@gmail.com>
2023-01-13 19:31:07 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# log_bash_history
+
+Log bash history to syslog server
+
+Steps 
+- Clone the repository
+- Replace syslog_IP with your syslog IP
+- run sh forward_bash_history.sh 
+
+
---
+++ forward_bash_history.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Author: Ali_Alwashali
+ 
+if test -f "/etc/bashrc"; then  
+    FILE=/etc/bashrc
+else 
+    FILE=/etc/bash.bashrc
+fi
+
+
+cat << EOF >> $FILE
+
+# Added: bash history logging
+export HISTTIMEFORMAT="%F %T "
+export PROMPT_COMMAND='logger -p local6.debug "\$(whoami) \$(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" ) [\$?]"'
+EOF
+
+
+
+# create log file and assign permission to syslog user
+mkdir /var/log/bash_history/
+touch /var/log/bash_history/bash_history.log
+chown syslog /var/log/bash_history/bash_history.log
+
+
+cat << EOF > /etc/rsyslog.d/bash_history_forwarding.conf
+local6.*    /var/log/bash_history/bash_history.log
+EOF
+
+# select the facility you want, *.* is very intense. authpriv.* could be a good start
+cat << EOF >> /etc/rsyslog.conf
+## logs fowarding to syslog server 
+*.*;local6.*                                          @192.168.111.111:514
+EOF
+
+
+cat << EOF > /etc/rsyslog.d/bash_history_forwarding.conf
+local6.*    /var/log/bash_history/bash_history.log
+EOF
+
+# rotate file
+cat << EOF >> /etc/logrotate.d/bash_history
+/var/log/bash_history/*.log {
+    maxsize 50M
+    hourly
+    missingok
+    rotate 8
+    compress
+    notifempty
+    nocreate
+}
+EOF
+
+
+sudo systemctl restart rsyslog.service
+
